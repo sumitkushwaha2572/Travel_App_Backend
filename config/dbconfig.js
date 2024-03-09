0f749ca366b1571ecbadefb4e983b504febe7037
Your Name <you@example.com>
2024-03-09 11:00:33 +0530

    lulli

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDB= async() =>
 {
     try{
-        await mongoose.connect(process.env.DATABASE_URL,
+        await mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }
            
     )
     }catch(err){
